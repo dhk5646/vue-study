bc230c3890cf5fc66a37c0fa93b8756702dc4ca3
dhk5646@gmail.com <>
2024-09-20 16:33:49 +0900

    테크블로그포스트 화면을 메인으로

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,19 +5,6 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: () => import('@/views/HomeView.vue')
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('@/views/AboutView.vue')
-    },
-    {
-      path: '/tech-blog-post',
       name: 'tech-blog-post',
       component: () => import('@/views/TechBlogPost.vue')
     }
